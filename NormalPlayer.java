7038ffcc6b672610bf0bbfb52ea96e0b01158b2d
subhamthegreat <39816945+subhamthegreat@users.noreply.github.com>
2018-06-06 18:42:47 -0700

    Update NormalPlayer.java

--- NormalPlayer.java
+++ NormalPlayer.java
@@ -6,6 +6,43 @@ public class NormalPlayer extends Player {
 
 
     }
+    @Override
+	public String printPlayer() {
+	    String result = "";
+	    
+            if (count <= 0) {
+
+               int result1 = money - record;
+	    
+	                System.out.println();
+	    
+	                if (result1 > 0) {
+	    
+                   System.out.println("---------Congratulations!---------" );
+	    
+                    result = "Total money won" + " $" + (result1 - count * initalMoney);
+	   
+	                } else if (result1 == 0) {
+	    
+                    result = " did not win nor did lose..";
+	    
+	                } else {
+	    
+                   result1 *= -1;
+	   	                    System.out.println("---------Better Luck next time!!--------- ");
+	    
+                    result = "Total money lost " + "$" + (result1 + count * initalMoney);
+	    
+               }
+	    
+            }
+	    
+            System.out.println(" Housemoney" +  ": $" + Game.houseMoney);
+	    
+           System.out.println();
+	    
+	            return result;
+	}
 
 
 
