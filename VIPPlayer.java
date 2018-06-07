992e0ef10c0d167ea8d93293818c03bacd2d18a3
subhamthegreat <39816945+subhamthegreat@users.noreply.github.com>
2018-06-06 18:41:34 -0700

    Update VIPPlayer.java

--- VIPPlayer.java
+++ VIPPlayer.java
@@ -20,6 +20,38 @@ public class VIPPlayer extends Player{
         return (int) Math.round(cashBackDouble); // convert to int and return
 
     }
+    @Override
+	public String printPlayer() {
+	       String result = "";
+	       
+              if (count <= 0) {
+	       
+                   int result1 = money - record;
+	                   System.out.println();
+	                          if (result1 > 0) {
+	       
+                  System.out.println("---------Congratulations!---------:" +name+"\tPlayer ID:"+idNumber);;
+	                              result = "Total money won" + " $" + (result1 - count * initalMoney);//need to work on it after no 3 of gameMenu
+	 	                   } else if (result1 == 0) {
+	                              result = " did not win nor did lose..";           } 
+	 	                   else {
+	       
+                   result1 *= -1;
+	       
+                       System.out.println("---------Better Luck next time!!--------- :"+name+"\tPlayer ID:"+idNumber);
+	       
+                       result = "Total money lost by " + name + " is $" + (result1 + count * initalMoney);
+	       	                   }
+	       
+               }
+	       	               System.out.println(" Housemoney because of " + name + ":$" + Game.houseMoney);
+	       
+             System.out.println();
+	       
+              return result;
+
+	           // method toString
+	}
 
 
 
